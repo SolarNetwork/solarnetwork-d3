6646ae6064c12d819925d7a79f46ebe425deeaa3
Matt Magoffin <git+matt@msqr.us>
2015-11-15 17:14:19 +1300

    Latest build; CQS app working.

--- solarnetwork-d3.js
+++ solarnetwork-d3.js
@@ -774,6 +774,63 @@
     return self;
   };
   sn.api.datum = {};
+  sn.api.datum.aggregateNestedDataLayers = sn_api_datum_aggregateNestedDataLayers;
+  /**
+ * Combine the layers resulting from a d3.nest() operation into a single, aggregated
+ * layer. This can be used to combine all sources of a single data type, for example
+ * to show all "power" sources as a single layer of chart data. The resulting object
+ * has the same structure as the input <code>layerData</code> parameter, with just a
+ * single layer of data.
+ * 
+ * @param {object} layerData - An object resulting from d3.nest().entries()
+ * @param {string} resultKey - The <code>key</code> property to assign to the returned layer.
+ * @param {array} copyProperties - An array of string property names to copy as-is from 
+ *                                 the <b>first</b> layer's data values.
+ * @param {array} sumProperties - An array of string property names to add together from
+ *                                <b>all</b> layer data.
+ * @param {object} staticProperties - Static properties to copy as-is to all output data.
+ * @return {object} An object with the same structure as returned by d3.nest().entries()
+ * @since 0.0.4
+ * @preserve
+ */
+  function sn_api_datum_aggregateNestedDataLayers(layerData, resultKey, copyProperties, sumProperties, staticProperties) {
+    var layerCount = layerData.length, dataLength, i, j, k, copyPropLength = copyProperties.length, sumPropLength = sumProperties.length, d, val, clone, array;
+    dataLength = layerData[0].values.length;
+    if (dataLength > 0) {
+      array = [];
+      for (i = 0; i < dataLength; i += 1) {
+        d = layerData[0].values[i];
+        clone = {};
+        if (staticProperties !== undefined) {
+          for (val in staticProperties) {
+            if (staticProperties.hasOwnProperty(val)) {
+              clone[val] = staticProperties[val];
+            }
+          }
+        }
+        for (k = 0; k < copyPropLength; k += 1) {
+          clone[copyProperties[k]] = d[copyProperties[k]];
+        }
+        for (k = 0; k < sumPropLength; k += 1) {
+          clone[sumProperties[k]] = 0;
+        }
+        for (j = 0; j < layerCount; j += 1) {
+          for (k = 0; k < sumPropLength; k += 1) {
+            val = layerData[j].values[i][sumProperties[k]];
+            if (val !== undefined) {
+              clone[sumProperties[k]] += val;
+            }
+          }
+        }
+        array.push(clone);
+      }
+      layerData = [ {
+        key: resultKey,
+        values: array
+      } ];
+    }
+    return layerData;
+  }
   sn.format = {};
   sn.format.dateTimeFormat = d3.time.format.utc("%Y-%m-%d %H:%M");
   sn.format.timestampFormat = d3.time.format.utc("%Y-%m-%d %H:%M:%S.%LZ");
@@ -4555,7 +4612,7 @@
         angle: a,
         angleStart: d.startAngle,
         angleEnd: d.endAngle,
-        degrees: sn.rad2deg(a),
+        degrees: sn.math.rad2deg(a),
         radius: r,
         innerRadius: innerRadius,
         center: [ (w + p[1] + p[3]) / 2, (h + p[0] + p[2]) / 2 ],
@@ -4908,6 +4965,30 @@
     };
     return self;
   };
+  sn.math = {
+    deg2rad: sn_math_deg2rad,
+    rad2deg: sn_math_rad2deg
+  };
+  /**
+ * Convert degrees to radians.
+ * 
+ * @param {number} deg - the degrees value to convert to radians
+ * @returns {number} the radians
+ * @preserve
+ */
+  function sn_math_deg2rad(deg) {
+    return deg * Math.PI / 180;
+  }
+  /**
+ * Convert radians to degrees.
+ * 
+ * @param {number} rad - the radians value to convert to degrees
+ * @returns {number} the degrees
+ * @preserve
+ */
+  function sn_math_rad2deg(rad) {
+    return rad * 180 / Math.PI;
+  }
   /**
  * An analog, circular gauge styled like a speedometer.
  *
@@ -4965,10 +5046,10 @@
       });
       arc = d3.svg.arc().innerRadius(r - config.ringWidth - config.ringInset).outerRadius(r - config.ringInset).startAngle(function(d, i) {
         var ratio = d * i;
-        return sn.deg2rad(config.minAngle + ratio * range);
+        return sn.math.deg2rad(config.minAngle + ratio * range);
       }).endAngle(function(d, i) {
         var ratio = d * (i + 1);
-        return sn.deg2rad(config.minAngle + ratio * range);
+        return sn.math.deg2rad(config.minAngle + ratio * range);
       });
     }
     that.configure = configure;
@@ -5898,30 +5979,6 @@
       formatted = formatted.replace(regexp, replaceValue);
     }
     return formatted;
-  }
-  sn.math = {
-    deg2rad: sn_math_deg2rad,
-    rad2deg: sn_math_rad2deg
-  };
-  /**
- * Convert degrees to radians.
- * 
- * @param {number} deg - the degrees value to convert to radians
- * @returns {number} the radians
- * @preserve
- */
-  function sn_math_deg2rad(deg) {
-    return deg * Math.PI / 180;
-  }
-  /**
- * Convert radians to degrees.
- * 
- * @param {number} rad - the radians value to convert to degrees
- * @returns {number} the degrees
- * @preserve
- */
-  function sn_math_rad2deg(rad) {
-    return rad * 180 / Math.PI;
   }
   sn.net.encodeURLQueryTerms = sn_net_encodeURLQueryTerms;
   /**
