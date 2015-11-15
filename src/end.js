bb07db0cc292868ddc2886042f0b74d307564c60
Matt Magoffin <git+matt@msqr.us>
2015-11-16 09:55:39 +1300

    Update wrapper to work within browser as-is as well as within ember-cli as a `import sn from "npm:solarnetwork-d3";`

--- src/end.js
+++ src/end.js
@@ -1,8 +1,2 @@
-	if ( typeof define === "function" && define.amd ) {
-		define(this.sn = sn);
-	} else if ( typeof module === "object" && module.exports ) {
-		module.exports = sn;
-	} else {
-		global.sn = sn;
-	}
-}();
+	return sn;
+}));
