edebcfd33d35f7881672f35050c4223d1ae9d958
Matt Magoffin <git+matt@msqr.us>
2015-12-07 10:49:38 +1300

    Yikes! Missed another typo.

--- src/config/config.js
+++ src/config/config.js
@@ -4,9 +4,9 @@ sn.config = {
 	debug : false,
 	host : 'data.solarnetwork.net',
 	tls : (function() {
-		return (global !== undefined 
-			&& global.locaion !== undefined
-			&& global.location.protocol !== undefined 
+		return (global !== undefined
+			&& global.location !== undefined
+			&& global.location.protocol !== undefined
 			&& global.location.protocol.toLowerCase().indexOf('https') === 0 ? true : false);
 	}()),
 	path : '/solarquery',
