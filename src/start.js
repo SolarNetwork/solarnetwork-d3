b7288ca2bc90c8ba925cdd0060a50c14ac458102
Matt Magoffin <git+matt@msqr.us>
2015-12-09 20:53:09 +1300

    Add missing CryptoJS imports.

--- src/start.js
+++ src/start.js
@@ -1,11 +1,11 @@
 (function (root, factory) {
 	if ( typeof define === "function" && define.amd ) {
-		define(["colorbrewer", "d3", "queue-async"], factory);
+		define(["colorbrewer", "d3", "queue-async", "crypto-js"], factory);
 	} else if ( typeof module === "object" && module.exports ) {
-		module.exports = factory(require("colorbrewer"), require("d3"), require("queue-async"));
+		module.exports = factory(require("colorbrewer"), require("d3"), require("queue-async"), require("crypto-js"));
 	} else {
-		root.sn = factory(root.colorbrewer, root.d3, root.queue);
+		root.sn = factory(root.colorbrewer, root.d3, root.queue, root.CryptoJS);
 	}
-}(this, function(colorbrewer, d3, queue) {
+}(this, function(colorbrewer, d3, queue, CryptoJS) {
 	'use strict';
-	var sn = {version: "0.9.9"};
+	var sn = {version: "0.10.0"};
