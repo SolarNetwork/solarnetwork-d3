12db9302d4e1ef9136872030cd78d9554223fb33
Matt Magoffin <git+matt@msqr.us>
2023-03-14 15:45:57 +1300

    Always default to using TLS.

--- src/config/config.js
+++ src/config/config.js
@@ -3,12 +3,7 @@ import "../core/global";
 var sn_config = {
 	debug : false,
 	host : 'data.solarnetwork.net',
-	tls : (function() {
-		return (global !== undefined
-			&& global.location !== undefined
-			&& global.location.protocol !== undefined
-			&& global.location.protocol.toLowerCase().indexOf('https') === 0 ? true : false);
-	}()),
+	tls : true,
 	path : '/solarquery',
 	solarUserPath : '/solaruser',
 	secureQuery : false
